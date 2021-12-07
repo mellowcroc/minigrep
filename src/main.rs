cc0842f622336737826e1c4e8ea3585cd88ae30a
dylan <mellowcroc@gmail.com>
2021-12-08 01:46:44 +0900

    Feat: Use iterators
    1. Use iterator in place of `clone`
    2. Use iterator trait methods instead of indexing
    3. Use iterator adaptors to make code clearer

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use std::fs;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
